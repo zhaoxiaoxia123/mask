28df3d877edaae96815eb8becc4c5af8859e43a9
axPhoenix <3427870036@qq.com>
2020-05-06 16:58:36 +0800

    x

--- pages/my/coupon/coupon.js
+++ pages/my/coupon/coupon.js
@@ -7,10 +7,30 @@ Page({
    * 组件的初始数据
    */
   data: {
+    currentIndex: 0,
     offset: 1,
     pageCount: 20,
     isLast: false,
     ticketList: []
+  },
+
+  //swiper切换时会调用
+  pagechange: function (e) {
+    if ("touch" === e.detail.source) {
+      let currentPageIndex = this.data.currentIndex
+      currentPageIndex = (currentPageIndex + 1) % 3
+      this.setData({
+        currentIndex: currentPageIndex
+      })
+    }
+  },
+  //用户点击tab时调用
+  titleClick: function (e) {
+    let currentPageIndex =
+      this.setData({
+        //拿到当前索引并动态改变
+        currentIndex: e.currentTarget.dataset.idx
+      })
   },
 
   /**
